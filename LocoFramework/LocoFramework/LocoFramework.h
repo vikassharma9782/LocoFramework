d105cbc5217608b7d4dc9cc3e1e9d1f99168d3cd
vikas sharma <vikas@lintelindia.com>
2019-12-18 14:01:33 +0530

    finished locoFramework

---
+++ LocoFramework/LocoFramework/LocoFramework.h
@@ -0,0 +1,19 @@
+//
+//  LocoFramework.h
+//  LocoFramework
+//
+//  Created by Harsh Vardhan on 18/12/19.
+//  Copyright © 2019 Harsh Vardhan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LocoFramework.
+FOUNDATION_EXPORT double LocoFrameworkVersionNumber;
+
+//! Project version string for LocoFramework.
+FOUNDATION_EXPORT const unsigned char LocoFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LocoFramework/PublicHeader.h>
+
+
---
+++ LocoFramework/LocoFramework/Service.swift
@@ -0,0 +1,17 @@
+//
+//  Service.swift
+//  LocoFramework
+//
+//  Created by Harsh Vardhan on 18/12/19.
+//  Copyright © 2019 Harsh Vardhan. All rights reserved.
+//
+
+import Foundation
+
+public class Service{
+    
+    private init(){}
+    public static func doSomething() -> String{
+        return "Hello, This is test pod";
+    }
+}
